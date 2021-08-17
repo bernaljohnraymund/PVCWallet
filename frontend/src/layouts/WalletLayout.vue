9183a851cde129df823ef8379f51b5037e455a9f
Don <bernaljohnraymund777@gmail.com>
2021-08-18 01:07:22 +0800

    feature: kyc

--- frontend/src/layouts/WalletLayout.vue
+++ frontend/src/layouts/WalletLayout.vue
@@ -24,30 +24,7 @@
       v-model="leftDrawerOpen"
       v-if="!noGlobalComponentRoutes.drawer.includes($route.name)"
     >
-      <div id="drawer-content">
-        <div id="profile" class="absolute-top text-center">
-          <img src="/temporary/couple-img.jpg" />
-          <h6 class="name absolute-bottom">{{ profile.firstName }} {{ profile.lastName }}</h6>
-        </div>
-        <div class="menu q-pa-md" style="max-width: 350px">
-          <q-list>
-            <q-item
-              clickable
-              v-ripple
-              v-for="(item, i) in menuItems"
-              :key="i"
-              @click="activeItem(item.name)"
-              :active="menu.item === item.name"
-              active-class="active"
-            >
-              <q-item-section avatar>
-                <q-icon :name="item.icon"  class="text-center" />
-              </q-item-section>
-              <q-item-section>{{ item.label }}</q-item-section>
-            </q-item>
-          </q-list>
-        </div>
-      </div>
+      <drawer-menu />
     </q-drawer>
 
     <q-page-container>
@@ -58,53 +35,17 @@
 
 <script>
 import noGlobalComponentRoutes from '../utils/data/noGlobalComponentRoutes'
+import DrawerMenu from '../components/global/DrawerMenu.vue'
 import { ref } from 'vue'
-
-let menuItems= [{
-      name: 'dashboard',
-      label: 'Dashboard',
-      icon: 'dashboard'
-    },
-    {
-      name: 'portfolio',
-      label: 'Portfolio',
-      icon: 'pie_chart'
-    }, {
-      name: 'transactions',
-      label: 'Transactions',
-      icon: 'receipt_long'
-    },
-    {
-      name: 'chart',
-      label: 'Chart',
-      icon: 'trending_up'
-    },
-    {
-      name: 'settings',
-      label: 'Settings',
-      icon: 'settings'
-    },{
-      name: 'logout',
-      label: 'Logout',
-      icon: 'logout'
-    }]
 
 export default {
   name: 'WalletLayout',
 
   components: {
+    DrawerMenu
   },
   data: () => ({
     noGlobalComponentRoutes: noGlobalComponentRoutes,
-    profile: {
-      firstName: "John Raymund",
-      middleName: "",
-      lastName: "Bernal",
-    },
-    menu: {
-      item: 'dashboard'
-    },
-    
   }),
 
   setup () {
@@ -112,7 +53,6 @@ export default {
     
     return {
       leftDrawerOpen,
-      menuItems,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
@@ -123,10 +63,6 @@ export default {
   },
 
   methods: {
-    async activeItem (menuItem) {
-      console.log(menuItem)
-      this.menu.item = menuItem;
-    }
   }
 }
 </script>
@@ -138,46 +74,6 @@ export default {
     color: #FBFBFB;
     header {
       background-color: #00a1ff;
-    }
-    #drawer-content {
-      background-color: #0b0c22 !important;
-      width: 100%;
-      height: 100%;
-    }
-    .q-toolbar .q-avatar {
-        left: 5px;
-    }
-    #profile {
-      width: 100%;
-      max-height: 244px;
-      height: 37vh;
-      padding-top: 7vh;
-      display: block;
-      position: relative;
-
-      img {
-        width: 50%;
-        border-radius: 50%;
-      }
-
-      .name {
-        margin: 0;
-      }
-    }
-    .menu {
-      position: relative;
-      margin-top: 7vh;
-
-      .active {
-        color: #FBFBFB;
-        background-image: linear-gradient(to right, #2D5EF5, #44B6F4);
-      }
-      .q-item__section--side > .q-icon {
-        background: -webkit-linear-gradient(left, #2D5EF5 2%, #44B6F4 100%);
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-      }
     }
     .q-page-container > div {
       padding: 20px;
